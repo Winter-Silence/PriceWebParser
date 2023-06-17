e205f943cc91c72eed6e48bd247fde0269566862
Yury <winter.silence6@gmail.com>
2023-06-18 00:18:06 +0400

    fix parser job

--- app/jobs/single_page_parser_job.rb
+++ app/jobs/single_page_parser_job.rb
@@ -2,14 +2,14 @@
 
 class SinglePageParserJob < ApplicationJob
   def perform(parse_now: false)
-    current_time = Time.now
+    current_time = Time.current
     threshold_time = current_time - 30.minutes
     products = Product.includes(product_parser_rules: :prices)
                       .where(product_parser_rules: { last_run: ..threshold_time })
     products.each do |product|
       product.product_parser_rules.each do |rule|
         rule.touch(:last_run)
-        parse_now ? SinglePageRuleJob.perform_now(product, rule) : SinglePageRuleJob.perform_async(product, rule)
+        parse_now ? SinglePageRuleJob.perform_now(rule) : SinglePageRuleJob.perform_later(rule)
       end
     end
   end
--- app/jobs/single_page_rule_job.rb
+++ app/jobs/single_page_rule_job.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class SinglePageRuleJob < ApplicationJob
-  def perform(product, parser_rule)
-    parser = Parser::ProductPageParser.new(parser_rule.url)
-    price_value = parser.get_value(parser_rule.selector)
-    return if price_value.nil? || (!product.lowest_price.nil? && product.lowest_price <= price_value)
+  def perform(rule)
+    parser = Parser::ProductPageParser.new(rule.url)
+    price_value = parser.get_value(rule.selector)
+    return  if price_value.nil? || (!rule.product.lowest_price.nil? && rule.product.lowest_price <= price_value)
 
-    price = Price.new(product_parser_rule: parser_rule)
+    price = Price.new(product_parser_rule: rule)
     price.value = price_value
     price.save
   end
