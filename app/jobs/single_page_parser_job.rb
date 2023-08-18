fcc6f9c9bf99a9e3bb8f18483d9efa2380a3a3a5
Winter_Silence <winter.silence6@gmail.com>
2023-08-18 08:05:58 +0400

    fix

--- app/jobs/single_page_parser_job.rb
+++ app/jobs/single_page_parser_job.rb
@@ -2,7 +2,7 @@
 
 class SinglePageParserJob < ApplicationJob
   def perform(parse_now: false)
-    rules = ProductParserRule.only_active.joins(:prices)
+    rules = ProductParserRule.only_active.includes(:prices)
                              .where(ProductParserRule.arel_table[:last_run].lt(30.minutes.ago)
                                       .or(ProductParserRule.arel_table[:last_run].eq(nil)))
     rules.each do |rule|
