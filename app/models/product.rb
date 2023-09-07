7fba4af9cea02170100d24206b6a302c1995cb79
Winter_Silence <winter.silence6@gmail.com>
2023-09-07 10:28:11 +0400

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,25 @@ class Product < ApplicationRecord
   has_many :product_parser_rules, dependent: :destroy
   has_many :prices, through: :product_parser_rules
 
-  def lowest_price
-    prices.minimum(:value)
+  def lowest_price(since: nil)
+    return unless since.nil? || valid_date?(since)
+
+    prices.where(created_at: since..).minimum(:value)
+  end
+
+  private
+
+  def valid_date?(value)
+    if value.is_a?(ActiveSupport::TimeWithZone) || value.is_a?(DateTime) ||
+       begin
+         value.is_a?(String) && DateTime.parse(value)
+       rescue ArgumentError
+         false
+       end
+      true
+    else
+      raise ArgumentError,
+            "Параметр 'since' должен быть типа DateTime или строкой с корректным форматом даты и времени."
+    end
   end
 end
--- lib/notifier/telegram_bot.rb
+++ lib/notifier/telegram_bot.rb
@@ -11,7 +11,7 @@ module Notifier
       def error_alert(rule_error)
         product_title = rule_error.product_parser_rule.product.title
         rule_edit_page_url = Rails.application.routes.url_helpers.product_parser_rule_url(
-          rule_error.product_parser_rule, only_path: false, host: 'parser.rutend.ru'
+          rule_error.product_parser_rule, only_path: false, host: Rails.application.credentials[:host]
         )
         text = "У '#{product_title}' по правилу #{rule_edit_page_url} много ошибок типа '#{rule_error.error_type}'"
         Telegram.bot.send_message(chat_id: Rails.application.credentials.telegram[:chat_id], text:)
