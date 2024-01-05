30f36ce54542dc1fca9517432f05f875a0b3fa17
Winter_Silence <winter.silence6@gmail.com>
2024-01-05 13:45:13 +0400

    Сделал сохранение цены по правилу, если до этого ни одной цены не было в базе

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_many :product_parser_rules, dependent: :destroy
   has_many :prices, through: :product_parser_rules
 
-  alias_attribute :rules, :product_parser_rules
+  alias rules product_parser_rules
 
   enum :period_lowest_price, { 'два месяца': 60,
                                месяц: 30,
